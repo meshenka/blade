97e9b85a0d608c894aeb53ce694ab793cdfcb3be
Sylvain Gogel <sylvain.gogel@gmail.com>
2014-11-24 23:35:38 +0100

    update logo and favicon

--- profiles/ac/themes/custom/flatac/templates/page.tpl.php
+++ profiles/ac/themes/custom/flatac/templates/page.tpl.php
@@ -10,15 +10,31 @@
 
 <div id="page">
 
-  <header class="header container" id="header" role="banner">
+  <header class="header container-fluid" id="header" role="banner">
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo logo" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="logo-link active header__logo-image" /></a>
-    <?php endif; ?>
+    <div class="row">
 
-    <?php print render($page['header']); ?>
+      <?php if ($logo): ?>
+        <div class="col-md-2">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo logo" id="logo">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="logo-link active header__logo-image" />
+          </a>
+        </div>
+      <?php endif; ?>
+      <?php if ($logo): ?>
+        <div class="col-md-8">
+            <?php print render($page['header']['menu_block_1']); ?>
+        </div>
+        <div class="col-md-2">
+            <?php print render($page['header']['search_form']); ?>
+        </div>
+      <?php else: ?>
+        <div class="col-md-12">
+            <?php print render($page['header']); ?>
+        </div>
+      <?php endif; ?>
 
+    </div>
   </header>
 
   <div id="main">
