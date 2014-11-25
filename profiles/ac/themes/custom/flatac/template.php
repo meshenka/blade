1cfcdf0fea5a64089275e7b582faad3b1ed868b0
Sylvain Gogel <sylvain.gogel@gmail.com>
2014-11-25 20:47:49 +0100

    start charting for home node

--- profiles/ac/themes/custom/flatac/template.php
+++ profiles/ac/themes/custom/flatac/template.php
@@ -8,3 +8,12 @@
  */
 
 include_once dirname(__FILE__).'/bootstrap/menu.inc';
+
+function flatac_preprocess_page(&$variables)
+{
+    //kpr($variables);
+    //no page title in home node
+    if (isset($variables['node']) && ($variables['node']->type === 'home')) {
+        $variables['title'] = false;
+    }
+}
