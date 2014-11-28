036b38df88ee13c880bbd0bd164f0e51671b575e
Sylvain Gogel <sylvain.gogel@gmail.com>
2014-11-28 18:39:21 +0100

    add ConfigurationRunner

---
+++ profiles/blade/lib/Drupal/blade/Configuration/ConfigurationRunner.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @package Blade
+ * @subpackage Profile
+ *
+ * A simple class to run a stack of ConfiguratorInterface
+ */
+
+namespace Drupal\blade\Configuration;
+
+class ConfigurationRunner
+{
+    private $configurators = [];
+
+    /**
+     * chainable
+     * add a configurator to the stack
+     * @param  ConfiguratorInterface $conf
+     * @return ConfigurationRunner   this instance
+     */
+    public function pipe(ConfiguratorInterface $conf)
+    {
+        $this->configurators[] = $conf;
+
+        return $this;
+    }
+
+    /**
+     *
+     * Run the configurator stack
+     * @return array array of log messages
+     */
+    public function run()
+    {
+        $output = [];
+        foreach ($this->configurators as $conf) {
+            $output = array_merge($output, $conf->configure());
+        }
+
+        return $output;
+    }
+}
